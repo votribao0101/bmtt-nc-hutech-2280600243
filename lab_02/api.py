6a6f4c5e9387f8268af4e960eebd47695b62a04f
votribao0101 <vbao0101@gmail.com>
2025-03-05 11:22:37 +0700

    add caesar vigenere railfence

--- lab_02/api.py
+++ lab_02/api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
 from cipher.vigenere import VignenereCipher
+from cipher.railfence import RailFenceCipher
 app = Flask(__name__)
 
 #caesar cipher
@@ -40,6 +41,27 @@ def vigenere_decrypt():
     key= data['key']
     decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text,key)
     return jsonify({'decrypted_text': decrypted_text})
+#Railfence
+
+railfence_cihper = RailFenceCipher()
+@app.route("/api/railfence/encrypt", methods=["POST"])
+def railfence_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key= int(data['key'])
+    encrypted_text = railfence_cihper.rail_fence_encypt(plain_text,key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route("/api/railfence/decrypt", methods=["POST"])
+def railfence_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key= int(data['key'])
+    decrypted_text = railfence_cihper.rail_fence_decrypt(cipher_text,key)
+    return jsonify({'decrypted_text': decrypted_text})
+
+
+
 #main
 if __name__ =="__main__":
     app.run(host="0.0.0.0", port=5000,debug=True)
--- lab_02/cipher/railfence/railfence_cipher.py
+++ lab_02/cipher/railfence/railfence_cipher.py
@@ -1,3 +1,47 @@
 class RailFenceCipher:
     def __init__(self):
-    def   
+        pass 
+    
+    def rail_fence_encypt(self, plain_text, num_rails):
+        rails =[[] for _ in range(num_rails)]
+        rail_index =0
+        direction =1 
+        for char in plain_text:
+            rails[rail_index].append(char)
+            if rail_index ==0:
+                direction=1
+            elif rail_index==num_rails-1:
+                direction=-1
+            rail_index+=direction
+        cipher_text =''.join(''.join(rail)for rail in rails)
+        return cipher_text
+    def rail_fence_decrypt(self, cipher_text,num_rails):
+        rail_lenghts =[0]*num_rails
+        rail_index =0
+        direction=1
+        
+        for _ in range(len(cipher_text)):
+            rail_lenghts[rail_index]+=1
+            if rail_index==0:
+                direction=1
+            elif rail_index ==num_rails-1:
+                direction =-1
+            rail_index += direction
+        rails=[]
+        start =0
+        for lenght in rail_lenghts:
+            rails.append(cipher_text[start:start+lenght])
+            start+=lenght 
+        plain_text=""
+        rail_index=0
+        direction =1
+        
+        for _ in range(len(cipher_text)):
+            plain_text+=rails[rail_index][0]
+            rails[rail_index] =rails[rail_index][1:]
+            if rail_index ==0:
+                direction =1;
+            elif rail_index == num_rails-1:
+                direction =-1
+            rail_index+= direction
+        return plain_text   
